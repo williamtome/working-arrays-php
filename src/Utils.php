c9111b33371f75ebe75b4290ac5673dcfa73cd53
William <william.tomenh@gmail.com>
2021-11-18 17:57:35 -0300

    feat: add strict types to validate the data parameters.

--- src/Utils.php
+++ src/Utils.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Alura\WorkingArrays;
 
 class Utils
 {
-    public static function remove(string $value, array &$array)
+    public static function remove(string|int $value, array &$array)
     {
-        $position = array_search($value, $array);
+        $position = array_search($value, $array, true);
         
         if (is_int($position)) {
             unset($array[$position]);
