836afd9cc0191bb0e8c4726152d3a38468cb80a2
William <william.tomenh@gmail.com>
2022-07-01 07:58:40 -0300

    feat: array is list.

---
+++ array-is-list.php
@@ -0,0 +1,11 @@
+<?php
+
+$students = [
+    'Ana' => 10,
+    'Claire' => 5,
+    'Joseph' => 9,
+    'William' => 1,
+];
+
+// This function it's works for PHP 8.1 version.
+echo array_is_list($students);
