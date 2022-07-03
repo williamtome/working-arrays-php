14ed6c4e9cd91b164df76ff4f3785cdab620c325
William <william.tomenh@gmail.com>
2022-07-02 21:21:35 -0300

    feat: use spread operator in arrays.

---
+++ spread-operator.php
@@ -0,0 +1,26 @@
+<?php
+
+$students2022 = [
+    'Anne with an E',
+    'William',
+    'John Snow',
+    'Luke Skywalker',
+    'Han Solo'
+];
+
+$newStudents = [
+    'Nico',
+    'Kilderson',
+    'Patrícia',
+    'Lucas',
+];
+
+$students = [...$students2022, ...$newStudents];
+var_dump($students);
+
+funcao(...[1, 2, 3]);
+
+function funcao(int $a, int $b, int $c)
+{
+    var_dump($a, $b, $c);
+}
